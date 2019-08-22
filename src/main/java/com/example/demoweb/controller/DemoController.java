5d1e8434ac3f29cd252eee9f5c656b1ee498b724
Andrey Karadzha <andrej.karadzha@privatbank.ua>
2019-08-22 14:10:51 +0300

    git init

--- src/main/java/com/example/demoweb/controller/DemoController.java
+++ src/main/java/com/example/demoweb/controller/DemoController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class DemoController {
+
     @GetMapping("/test")
     public ResponseDto test(){
         return ResponseDto.builder()
